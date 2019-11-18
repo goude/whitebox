0fd078341304efcd1d464224c66e5f7bc175abb1
Daniel Goude <daniel@goude.se>
2019-11-18 14:22:23 +0100

    board peg test

--- measurements.py
+++ measurements.py
@@ -1,4 +1,5 @@
 wall_thickness = 3.0
+
 # note: rpi 4
 rpi_board_width = 85.0
 rpi_board_depth = 65.0
@@ -7,6 +8,7 @@ rpi_cylinder = 6.0
 rpi_hole = 2.7
 rpi_hole_x_dist = 58.0
 rpi_hole_y_dist = 49.0
+
 enclosure_height = 10.0
 margin = 10.0
 enclosure_width = 110.0  # rpi_board_width + margin
